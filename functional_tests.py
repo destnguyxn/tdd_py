d0e46e9ace22601baeee891e30338e0392a68a32
erudilumos <chromevi123@gmail.com>
2022-11-14 22:54:58 +0700

    feat: django ORM, add item model

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,6 @@ class NewVisitorTest(unittest.TestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
 
         # There is still a text box inviting her to add another item. She
@@ -46,6 +45,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME,'tr')
